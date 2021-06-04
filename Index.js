191fe0b3740c73ba2a17d0de4032deaf75c164cb
Bjorn Sveia <bjorn@radalabs.net>
2021-06-04 09:52:40 +0200

    update

--- Index.js
+++ Index.js
@@ -5,15 +5,17 @@ var contentDiv = document.getElementById("content");
 let numbers = [7, 3, 1, 5, 8];
 let chosenBar; // Variabel for hvilken stolpe som er valgt
 let inputValue; // Variabel for hva som er skrevet i input-feltet
+let borderCol;
 // view
 show();
 function show() {
+  console.log(chosenBar);
   let svgInnerHtml = "";
   for (let i = 0; i < numbers.length; i++) {
     svgInnerHtml += createBar(numbers[i], i + 1);
   }
   contentDiv.innerHTML = `
-         <svg id="chart" width="500" viewBox="0 0 80 60">
+         <svg  id="chart" width="500" viewBox="0 0 80 60">
              ${svgInnerHtml}
          </svg><br/>
          Valgt stolpe: <i>ingen</i>
@@ -21,8 +23,8 @@ function show() {
          Verdi:
          <input type="number" min="1" max="10" oninput="inputValue = this.value" />
          <button onclick="addBar(inputValue)">Legg til stolpe</button>
-         <button disabled>Endre valgt stolpe</button><br />
-         <button onclick="removeBar(2)">Fjerne valgt stolpe</button>
+         <button onclick="editBar()" >Endre valgt stolpe</button><br />
+         <button onclick="removeBar()">Fjerne valgt stolpe</button>
          `;
 }
 
@@ -33,8 +35,8 @@ function createBar(number, barNo) {
   let height = number * 10;
   let y = 60 - height;
   let color = calcColor(1, 10, barNo);
-  return `<rect width="${width}" height="${height}"
-                         x="${x}" y="${y}" fill="${color}"></rect>`;
+  return `<rect style="border: blue solid 1px" onclick="activateBar(this)" width="${width}" height="${height}"
+                         x="${x}" y="${y}" fill="${color} "  ></rect>`;
 }
 
 function calcColor(min, max, val) {
@@ -46,18 +48,36 @@ function calcColor(min, max, val) {
   return colString;
 }
 
+// class barController
 // controller
 function addBar(val) {
   parsed = parseInt(val);
-  numbers.push(parsed);
+  parsed
+    ? (numbers.push(parsed), (inputValue = null))
+    : alert("Please specify the bar you want to create");
   console.log(numbers);
   show();
 }
-function removeBar(val) {
-  let i = parseInt(val);
-  let filtered = numbers.filter((item) => item !== i);
-  numbers = filtered;
-  console.log(filtered);
+function editBar() {
+  chosenBar && inputValue
+    ? ((numbers[chosenBar] = inputValue),
+      (chosenBar = null),
+      (inputValue = null))
+    : (null, alert("no input value or chosen bar"));
   show();
+}
+//
+function removeBar() {
+  chosenBar ? numbers.splice(chosenBar, 1) : alert("No bar is chosen");
+  chosenBar = null;
+  show();
+}
+function activateBar(value) {
+  chosenBar !== [...value.parentElement.children].indexOf(value)
+    ? (chosenBar = [...value.parentElement.children].indexOf(value))
+    : (chosenBar = null);
+  console.log(chosenBar);
+  // console.log([...value.parentElement.children].indexOf(value));
+  // show();
   //   numbers.slice(item);
 }
